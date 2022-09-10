50613705ba8f536ddf7726aa7f62438eceebac50
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-09-10 10:57:58 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1 title="web dev made simple">Hello World</h1>;
+const root = document.getElementById("root");
+ReactDOM.render(element, root);
