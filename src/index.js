6a3cc5d3c55dd576c672f0b30da111a835eca18c
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-09-11 10:43:21 +0300

    basic rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-const element = <h1 title="web dev made simple">Hello World</h1>;
+const MyReact = {
+  createElement,
+  render,
+};
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        if (typeof child === "object") {
+          return child;
+        }
+        return createTextElement(child);
+      }),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  Object.keys(element.props)
+    .filter((k) => (k !== "children" ? true : false))
+    .forEach((propName) => (dom[propName] = element.props[propName]));
+
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
+}
+
+/** @jsx MyReact.createElement */
+const element = (
+  <div style="background: orange; color:white;">
+    <h1 title="web dev made simple">Hello World</h1>
+  </div>
+);
 const root = document.getElementById("root");
-ReactDOM.render(element, root);
+MyReact.render(element, root);
