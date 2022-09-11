68d48ce6b120c2513f326cd05177f739303a274d
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2022-09-11 23:12:01 +0300

    fiber

--- src/index.js
+++ src/index.js
@@ -31,21 +31,28 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
-  Object.keys(element.props)
+      : document.createElement(fiber.type);
+  Object.keys(fiber.props)
     .filter((k) => (k !== "children" ? true : false))
-    .forEach((propName) => (dom[propName] = element.props[propName]));
+    .forEach((propName) => (dom[propName] = fiber.props[propName]));
 
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+  return dom;
 }
 
 let nextUnitOfWork = null;
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -59,9 +66,52 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop);
 }
 
-function performUnitOfWork() {
-  // do some work
-  // return the next unit of work
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+
+  let index = 0;
+
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 requestIdleCallback(workLoop);
